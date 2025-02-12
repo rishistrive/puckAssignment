e89c26092df25964862428b4d629527ca14a4f47
rishistrive <rishidevtech09@gmail.com>
2025-02-12 15:14:13 +0530

    added form

--- my-app/puck.config.tsx
+++ my-app/puck.config.tsx
@@ -2,7 +2,7 @@ import type { Config } from "@measured/puck";
 
 type Props = {
   HeadingBlock: { title: string };
-  Form: { title: string };
+  Form: { title: string; squares: string; radiosCheck: number; checkBoxtype: number };
 };
 
 export const config: Config<Props> = {
@@ -23,14 +23,29 @@ export const config: Config<Props> = {
     Form: {
       fields: {
         title: { type: "text" },
+        squares: { type: "textarea" },
+        radiosCheck: { type: "number" },
+        checkBoxtype: { type: "text" }, 
       },
       defaultProps: {
         title: "Form",
+        squares: "Default Text",
+        radiosCheck: 1,
+        checkBoxtype: 8, 
       },
-      render: ({ title }) => (
+      render: ({ title, squares, radiosCheck, checkBoxtype }) => (
         <div style={{ padding: 32 }}>
           <h2>{title}</h2>
+          <p>Squares: {squares}</p>
+          <p>Radios: {radiosCheck}</p>
+          <label>
+            <input type="radio"  readOnly />{radiosCheck}
+          </label>
+          <label><br></br>
+            <input type="checkbox"  readOnly />{checkBoxtype}
+          </label>
         </div>
+        
       ),
     },
   },
